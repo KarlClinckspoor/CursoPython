32098f5566731e585b9a0ee9c4de0ea1a8ef13ff
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2023-06-10 14:41:53 -0300

    Expandi operações matemáticas e bit-a-bit, reformatei algumas tabelas. Adicionei um exercício bobo.

---
+++ Capítulos/Capítulo01/Soluções dos exercícios/complex_mod.py
@@ -0,0 +1,7 @@
+class myComplex(complex):
+    def __mod__(self, other):
+        return myComplex(self.real % other.real, self.imag % other.imag)
+    def __floordiv__(self, other):
+        return myComplex(self.real // other.real, self.imag // other.imag)
+print(myComplex(10, 10) % (7 + 6j))
+print(myComplex(10, 10) // (7 + 6j))
