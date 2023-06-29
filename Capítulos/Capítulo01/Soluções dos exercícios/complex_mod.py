23feef965cbdfcd1e56d6dff995f39e2f2009534
karl.clinckspoor <karl.clinckspoor@lnnano.cnpem.br>
2023-06-29 14:57:08 -0300

    Apliquei black em todos os notebooks e arquivos .py

--- Capítulos/Capítulo01/Soluções dos exercícios/complex_mod.py
+++ Capítulos/Capítulo01/Soluções dos exercícios/complex_mod.py
@@ -1,7 +1,10 @@
 class myComplex(complex):
     def __mod__(self, other):
         return myComplex(self.real % other.real, self.imag % other.imag)
+
     def __floordiv__(self, other):
         return myComplex(self.real // other.real, self.imag // other.imag)
+
+
 print(myComplex(10, 10) % (7 + 6j))
 print(myComplex(10, 10) // (7 + 6j))
--- Capítulos/Capítulo01/Soluções dos exercícios/pint.py
+++ Capítulos/Capítulo01/Soluções dos exercícios/pint.py
@@ -1,9 +1,10 @@
 import pint
-ureg = pint.UnitRegistry(autoconvert_offset_to_baseunit = True)
 
-n = (3.4 * ureg.g / (4 * ureg.g / ureg.mole))
+ureg = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
+
+n = 3.4 * ureg.g / (4 * ureg.g / ureg.mole)
 R = 8.314472 * ureg.J / ureg.K / ureg.mole
 T = ureg.Quantity(77, ureg.degF)
 V = 35 * ureg.milliliters
 p = n * R * T / V
-p.to('bar')
+p.to("bar")
